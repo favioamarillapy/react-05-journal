c6d57c89377f424f50d6f61b5d8110f6c14884a3
FavioAmarilla <amarillafavio@gmail.com>
2021-08-22 16:11:44 -0400

    Added auth styles

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { JournalApp } from './JournalApp';
 
-import './assets/styles/base.scss';
+import './assets/styles/style.scss';
 
 ReactDOM.render(
   <JournalApp />,
